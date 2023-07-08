daf1fb582dfaa901a108eba11191f3c7b4564c44
sslxw <slxw191@gmail.com>
2023-07-08 16:28:02 +0300

    first commit

---
+++ newsletter.js
@@ -0,0 +1,65 @@
+const https = require("https")
+const bodyParser = require("body-parser")
+const express = require("express")
+
+const app = express()
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(express.static("public"))
+
+app.listen(procces.env.PORT || 3000, function(){
+    console.log("port is running on port 3000")
+});
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+})
+
+app.post("/error", function(req,res){
+    res.redirect("/")
+})
+
+app.post("/", function(req,res){
+    const firstName = req.body.firstName
+    const lastName = req.body.lastName
+    const email = req.body.email
+
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    }
+
+    const jsonData = JSON.stringify(data)
+    
+    const url = "https://us17.api.mailchimp.com/3.0/lists/1f051bc636"
+
+    const options = {
+        method: "POST",
+        auth: "sloohe191:5901cac861195d299be61b96f4ee0e7b-us17"
+    }
+
+    const request = https.request(url, options, function(response){
+        
+        if(response.statusCode === 200){
+            res.sendFile(__dirname + "/success.html")
+        } else{
+            res.sendFile(__dirname + "/error.html")
+        }
+        
+        response.on("data", function(data){
+            console.log(JSON.parse(data))
+        })
+    })
+
+    request.write(jsonData)
+    request.end()
+})
+
